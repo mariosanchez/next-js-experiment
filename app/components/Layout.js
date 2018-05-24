c8aea905cfd52ecfae03c71e271deb826fb23100
Mario Sánchez <msanchez@trovit.com>
2018-05-24 17:34:12 +0200

    Add Emotion for styles

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,13 +1,32 @@
 import Header from "./Header";
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: "1px solid #DDD"
-};
+import React from 'react'
+import { hydrate, injectGlobal } from 'react-emotion'
+
+// Adds server generated styles to emotion cache.
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  hydrate(window.__NEXT_DATA__.ids)
+}
+
+injectGlobal`
+  html, body {
+    padding: 1rem 1rem;
+    margin: 0;
+    background: rebeccapurple;
+    min-height: 100%;
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 24px;
+    color: white;
+
+    a {
+      color: yellow;
+    }
+  }
+`
 
 const Layout = props => (
-  <div style={layoutStyle}>
+  <div>
     <Header />
     {props.children}
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import Layout from "../app/components/Layout";
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
+import styled, { hydrate, keyframes, css } from 'react-emotion'
+
+const BeerItemImage = styled.img`
+  max-width: 100px;
+  max-height: 200px;
+`
 
 const Index = ({ beers }) => (
   <Layout>
     <h1>Punk API</h1>
     <h2>Beer list</h2>
-    <p>Index</p>
     { beers.map(beer => (
       <Link as={`/beer/${beer['id']}`} href={`/beer?id=${beer['id']}`} key={beer['name']}> 
         <a>
           <h3>{beer['name']}</h3>
-          <img src={beer['image_url']} alt={beer['name']}/>
+          <BeerItemImage src={beer['image_url']} alt={beer['name']}/>
         </a>
       </Link>
     ))}
